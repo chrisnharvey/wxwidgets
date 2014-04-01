05efef04b9923fb7639ad54a36f0227b3e3e0898
Chris Harvey <chris@chrisnharvey.com>
2014-04-01 21:12:18 +0100

    Updating service provider to allow new registration by single binding

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -7,20 +7,49 @@ use Encore\Wx\Command\Install as InstallCommand;
 
 class ServiceProvider extends \Encore\Container\ServiceProvider
 {
-    public function register()
+    public function register($binding)
     {
         if ( ! extension_loaded('wxwidgets')) {
             dl('wxwidgets.'.PHP_SHLIB_SUFFIX);
         }
 
-        $this->container->bind('view.composer', function() {
+        switch ($binding) {
+            case 'view.engine':
+                $this->registerViewEngine();
+            break;
+
+            case 'wx':
+                $this->registerWx();
+            break;
+
+            case 'launcher':
+                $this->registerLauncher();
+            break;
+
+            default: 
+                $this->registerViewEngine();
+                $this->registerWx();
+                $this->registerLauncher();
+            break;
+        }
+    }
+
+    protected function registerViewEngine()
+    {
+        $this->container->bind('view.engine', function() {
             $finder = new FileResourceFinder(new Filesystem, $this->app['config']['view.paths']);
 
             return new Manager($finder);
         });
+    }
 
+    protected function registerWx()
+    {
         $this->container->bind('wx', new Widgets);
+    }
 
+    protected function registerLauncher()
+    {
         $this->container->bind('launcher', function() {
             \wxApp::SetInstance($this->container['wx']);
             wxEntry();
@@ -34,6 +63,6 @@ class ServiceProvider extends \Encore\Container\ServiceProvider
 
     public function provides()
     {
-        return ['wx', 'launcher', 'view.composer', 'giml.collection'];
+        return ['wx', 'launcher', 'view.engine', 'giml.collection'];
     }
 }
