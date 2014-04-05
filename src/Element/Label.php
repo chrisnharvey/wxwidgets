5312d8088f0669c3c6f1f3ecf87314347dccb51a
Chris Harvey <chris@chrisnharvey.com>
2014-04-05 21:54:19 +0100

    Using positionable and sizeable traits

--- src/Element/Label.php
+++ src/Element/Label.php
@@ -5,12 +5,14 @@ namespace Encore\Wx\Element;
 class Label implements \Encore\GIML\ElementInterface
 {
     use \Encore\GIML\ElementTrait;
+    use Traits\Sizable;
+    use Traits\Positionable;
     use Traits\Wx;
 
     public function setParent(\Encore\GIML\ElementInterface &$parent)
     {
-        $this->element = new \wxStaticText($parent->getParent()->getRaw(), wxID_ANY, $this->value, wxDefaultPosition, wxDefaultSize);
-        $this->parent = $parent;
+        $this->element = new \wxStaticText($parent->getParent()->getRaw(), wxID_ANY, $this->value, $this->getPosition(), $this->getSize());
+        $this->parent =& $parent;
         $parent->getRaw()->Add($this->element);
     }
 }
