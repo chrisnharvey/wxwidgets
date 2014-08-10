1430274e65eb227dfd6c20160c746dd85d7951ae
Chris Harvey <chris@chrisnharvey.com>
2014-08-10 12:22:44 +0100

    Adding menu item object

---
+++ src/Element/MenuItem.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Encore\Wx\Element;
+
+class MenuItem implements \Encore\GIML\ElementInterface
+{
+    use \Encore\GIML\ElementTrait;
+    use Traits\Events;
+    use Traits\Wx;
+
+    protected $events = [
+        'onSelect' => wxEVT_COMMAND_MENU_SELECTED
+    ];
+
+    public function setParent(\Encore\GIML\ElementInterface $parent)
+    {
+        $this->parent = $parent;
+
+        if ($this->type == 'separator') {
+            return $parent->getRaw()->AppendSeparator();
+        }
+
+        $id = $this->collection->getTrueId($this->id);
+
+        $title = "&{$this->title}";
+
+        if ($this->shortcut) $title .= "\t{$this->shortcut}";
+
+        $this->element = new \wxMenuItem($parent->getRaw(), wxID_ANY, $title, $this->description, wxITEM_NORMAL);
+
+        $this->bindEvents();
+        
+        $parent->getRaw()->Append($this->element);
+    }
+
+    protected function getType()
+    {
+        switch ($this->type) {
+            case 'about':
+                return wxID_ABOUT;
+
+            case 'exit':
+                return wxID_EXIT;
+
+            case 'preferences':
+                return wxID_PREFERENCES;
+
+            case 'help':
+                return wxID_HELP;
+
+            default:
+                return wxID_ANY;
+        }
+    }
+}
