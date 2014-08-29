42751bc36bfd19b9d284fbfdc730c7fdd0588933
Chris Harvey <chris@chrisnharvey.com>
2014-08-29 18:59:09 +0100

    Adding events to TextBox

--- src/Element/TextBox.php
+++ src/Element/TextBox.php
@@ -8,6 +8,16 @@ class TextBox implements \Encore\GIML\ElementInterface
     use Traits\Wx;
     use Traits\Sizable;
     use Traits\Positionable;
+    use Traits\Events;
+
+    protected $events = [
+        'onCut' => wxEVT_COMMAND_TEXT_CUT,
+        'onEnter' => wxEVT_COMMAND_TEXT_ENTER,
+        'onMaxLen' => wxEVT_COMMAND_TEXT_MAXLEN,
+        'onPaste' => wxEVT_COMMAND_TEXT_PASTE,
+        'onUpdated' => wxEVT_COMMAND_TEXT_UPDATED,
+        'onUrl' => wxEVT_COMMAND_TEXT_URL
+    ];
 
     public function setParent(\Encore\GIML\ElementInterface $parent)
     {
@@ -16,6 +26,8 @@ class TextBox implements \Encore\GIML\ElementInterface
         $id = $this->collection->getTrueId($this->id);
 
         $this->element = new \wxTextCtrl($parent->getParent()->getRaw(), $id, $this->value or wxEmptyString, $this->getPosition(), $this->getSize(), 0);
+
+        $this->bindEvents();
 
         $parent->getRaw()->Add($this->element);
     }
