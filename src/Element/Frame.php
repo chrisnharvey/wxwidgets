19899536fabec8b49ed4c7b21b73b189fe8c536b
Chris Harvey <chris@chrisnharvey.com>
2014-09-04 20:31:50 +0100

    Adding events to frame

--- src/Element/Frame.php
+++ src/Element/Frame.php
@@ -10,10 +10,21 @@ class Frame implements GIML\ElementInterface
     use Traits\Wx;
     use Traits\Positionable;
     use Traits\Sizable;
+    use Traits\Events;
+
+    protected $events = [
+        'onClose' => wxEVT_CLOSE_WINDOW,
+        'onMinimize' => wxEVT_ICONIZE,
+        'onMenuOpen' => wxEVT_MENU_OPEN,
+        'onMenuClose' => wxEVT_MENU_CLOSE,
+        'onMenuHighlight' => wxEVT_MENU_HIGHLIGHT,
+    ];
 
     public function init()
     {
         $this->element = new \wxFrame(null, wxID_ANY, $this->title, $this->getPosition(), $this->getSize(), wxDEFAULT_FRAME_STYLE|wxTAB_TRAVERSAL);
+
+        $this->bindEvents();
     }
 
     public function show()
