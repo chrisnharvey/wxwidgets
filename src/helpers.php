fdab121b394af7d00251baa9abae0577c0320900
Chris Harvey <chris@chrisnharvey.com>
2014-09-07 21:33:55 +0100

    Only define the helper functions if they do not already exist

--- src/helpers.php
+++ src/helpers.php
@@ -1,18 +1,24 @@
 <?php
 
-function alert($message, $title = 'Alert')
-{
-    return \wxMessageBox($message, $title, wxOK|wxICON_INFORMATION);
+if ( ! function_exists('alert')) {
+    function alert($message, $title = 'Alert')
+    {
+        return \wxMessageBox($message, $title, wxOK|wxICON_INFORMATION);
+    }
 }
 
-function error($message, $title = 'Error')
-{
-    return \wxMessageBox($message, $title, wxOK|wxICON_ERROR);
+if ( ! function_exists('error')) {
+    function error($message, $title = 'Error')
+    {
+        return \wxMessageBox($message, $title, wxOK|wxICON_ERROR);
+    }
 }
 
-function confirm($message, $title = 'Confirm')
-{
-    $answer = \wxMessageBox($message, $title, wxYES_NO);
+if ( ! function_exists('confirm')) {
+    function confirm($message, $title = 'Confirm')
+    {
+        $answer = \wxMessageBox($message, $title, wxYES_NO);
 
-    return $answer === wxYES;
+        return $answer === wxYES;
+    }
 }
