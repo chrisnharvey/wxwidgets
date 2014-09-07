786ca79994638defe5a486fc77fece00bb177678
Chris Harvey <chris@chrisnharvey.com>
2014-09-07 20:52:36 +0100

    Forwarding calls to undefined methods to the raw element if the method exists in that object

--- src/Element/Traits/Wx.php
+++ src/Element/Traits/Wx.php
@@ -15,4 +15,13 @@ trait Wx
             $this->element->Destroy();
         }
     }
+
+    public function __call($method, $args)
+    {
+        if (method_exists($this->element, $method)) {
+            return call_user_func_array([$this->element, $method], $args);
+        }
+
+        // Trigger an error, the method does not exist
+    }
 }
