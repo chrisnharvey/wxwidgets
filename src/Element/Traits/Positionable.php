63221c0e41b0d121caa120635ae730cb14e810cf
Chris Harvey <chris@chrisnharvey.com>
2014-10-03 20:21:55 +0100

    Import wxPoint after namespace declaration

--- src/Element/Traits/Positionable.php
+++ src/Element/Traits/Positionable.php
@@ -2,12 +2,14 @@
 
 namespace Encore\Wx\Element\Traits;
 
+use wxPoint;
+
 trait Positionable
 {
     protected function getPosition()
     {
         if ($this->x and $this->y) {
-            return new \wxPoint($this->x, $this->y);
+            return new wxPoint($this->x, $this->y);
         }
 
         return wxDefaultPosition;
