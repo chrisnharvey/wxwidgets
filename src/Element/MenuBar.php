f4a150de85a230dae52cdd07114689c89afcaaf1
Chris Harvey <chris@chrisnharvey.com>
2014-10-03 20:34:45 +0100

    Cleaning up code by importing classes into this namespace

--- src/Element/MenuBar.php
+++ src/Element/MenuBar.php
@@ -2,17 +2,22 @@
 
 namespace Encore\Wx\Element;
 
+use wxMenuBar;
+use Encore\Giml\ElementTrait;
+use Encore\Wx\Element\Traits\Wx;
+use Encore\Giml\ElementInterface;
+
 class MenuBar implements \Encore\Giml\ElementInterface
 {
-    use \Encore\Giml\ElementTrait;
+    use ElementTrait;
     use Traits\Wx;
 
     public function init()
     {
-        $this->element = new \wxMenuBar(0);
+        $this->element = new wxMenuBar(0);
     }
 
-    public function setParent(\Encore\Giml\ElementInterface $parent)
+    public function setParent(ElementInterface $parent)
     {
         $this->parent = $parent; 
 
