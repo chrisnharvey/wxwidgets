e82a0df9bc44924c3e8f149a84ed43b02b768c57
Chris Harvey <chris@chrisnharvey.com>
2014-10-03 20:22:59 +0100

    Import wxSize after namespace declaration

--- src/Element/Traits/Sizable.php
+++ src/Element/Traits/Sizable.php
@@ -2,12 +2,14 @@
 
 namespace Encore\Wx\Element\Traits;
 
+use wxSize;
+
 trait Sizable
 {
     protected function getSize()
     {
         if ($this->width and $this->height) {
-            return new \wxSize($this->width, $this->height);
+            return new wxSize($this->width, $this->height);
         }
 
         return wxDefaultSize;
