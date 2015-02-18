a91a633f27c6be90c28f48d86115a0e481819829
Chris Harvey <chris@chrisnharvey.com>
2015-02-18 22:51:05 +0000

    Allowing a menu to be passed into raw task bar icon so we can create a popup menu

--- src/Element/Raw/TaskBarIcon.php
+++ src/Element/Raw/TaskBarIcon.php
@@ -2,7 +2,21 @@
 
 namespace Encore\Wx\Element\Raw;
 
+use Encore\Giml\ElementInterface;
+
 class TaskBarIcon extends \wxTaskBarIcon
 {
+    protected $menu;
 
+    public function setMenu(ElementInterface $menu)
+    {
+        $this->menu = $menu;
+    }
+
+    public function CreatePopupMenu()
+    {
+        if ( ! isset($this->menu)) return new \wxMenu;
+
+        return $this->menu->getRaw();
+    }
 }
